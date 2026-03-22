42689954a09d738caf5de2197c6bf36ef7629d52
Niharika Nikhare <niharikanikhare2005@gmail.com>
2026-03-22 23:52:07 +0530

    Improved accuracy of the model to 86%

--- app.py
+++ app.py
@@ -1,30 +1,59 @@
 import streamlit as st
 import numpy as np
+import os
 from tensorflow.keras.models import load_model
 from utils import extract_features
+from pydub import AudioSegment
 
 model = load_model("deepfake_detector.h5")
 
-st.title("🎤 AI Deepfake Audio Detector")
+st.title("AI Deepfake Audio Detector")
 
-
-uploaded_file = st.file_uploader("Upload audio file", type=["wav"])
+uploaded_file = st.file_uploader("Upload audio file", type=["wav", "mp3", "mp4"])
 
 if uploaded_file is not None:
 
-    with open("temp.wav", "wb") as f:
+    input_path = "input_audio"
+    
+    file_ext = uploaded_file.name.split(".")[-1].lower()
+    input_file = f"{input_path}.{file_ext}"
+
+    with open(input_file, "wb") as f:
         f.write(uploaded_file.read())
 
-    st.audio(uploaded_file)
-
-    features = extract_features("temp.wav")
-    features = features[np.newaxis, ..., np.newaxis]
-
-
-    prediction = model.predict(features)[0][0]
-
-
-    if prediction > 0.5:
-        st.error(f"Fake Voice ({prediction*100:.2f}%)")
+    if file_ext in ["mp3", "mp4"]:
+        audio = AudioSegment.from_file(input_file)
+        wav_path = "converted_audio.wav"
+        audio.export(wav_path, format="wav")
+        final_path = wav_path
     else:
-        st.success(f"Real Voice ({(1-prediction)*100:.2f}%)")
+        final_path = input_file
+
+    st.audio(final_path)
+
+    try:
+        features = extract_features(final_path)
+
+        if features is None:
+            st.error("Could not process audio")
+        else:
+            features = features[np.newaxis, ..., np.newaxis]
+
+            prediction = model.predict(features)[0][0]
+
+            st.write(f"Confidence Score: {prediction:.4f}")
+
+            if prediction > 0.55:
+                st.error("Fake Voice Detected")
+            else:
+                st.success("Real Voice Detected")
+
+    except Exception as e:
+        st.error(f"Error: {e}")
+
+    try:
+        os.remove(input_file)
+        if os.path.exists("converted_audio.wav"):
+            os.remove("converted_audio.wav")
+    except:
+        pass
--- utils.py
+++ utils.py
@@ -1,28 +1,29 @@
 import librosa
 import numpy as np
+import cv2
 
 def extract_features(file_path, max_len=128):
     try:
-        audio, sr = librosa.load(file_path, sr=22050)
-        audio = librosa.util.normalize(audio)
+        y, sr = librosa.load(file_path)
 
-        mel = librosa.feature.melspectrogram(
-            y=audio,
-            sr=sr,
-            n_mels=128,
-            fmax=8000
-        )
+        y, _ = librosa.effects.trim(y)
 
+        target_len = sr * 3
+
+        if len(y) < target_len:
+            y = np.pad(y, (0, target_len - len(y)))
+        else:
+            y = y[:target_len]
+
+        mel = librosa.feature.melspectrogram(y=y, sr=sr)
         mel_db = librosa.power_to_db(mel, ref=np.max)
 
         mel_db = (mel_db - np.mean(mel_db)) / (np.std(mel_db) + 1e-6)
 
-        if mel_db.shape[1] < max_len:
-            mel_db = np.pad(mel_db, ((0,0),(0,max_len - mel_db.shape[1])))
-        else:
-            mel_db = mel_db[:, :max_len]
+        mel_db = cv2.resize(mel_db, (128, 128))
 
         return mel_db
 
-    except:
+    except Exception as e:
+        print("Error processing:", file_path, e)
         return None
