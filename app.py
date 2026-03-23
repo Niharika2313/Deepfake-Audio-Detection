7d9def8fa46af66d595232eb987877d355ac0661
Niharika Nikhare <niharikanikhare2005@gmail.com>
2026-03-23 22:12:26 +0530

    Improved streamlit UI features

--- app.py
+++ app.py
@@ -4,17 +4,21 @@ import os
 from keras.models import load_model
 from utils import extract_features
 from pydub import AudioSegment
+import librosa
+import matplotlib.pyplot as plt
+
+st.set_page_config(page_title="Deepfake Audio Detector", layout="centered")
 
 model = load_model("deepfake_detector.h5")
 
 st.title("AI Deepfake Audio Detector")
+st.markdown("Upload an audio file to check whether it's **Real or AI-generated**.")
 
-uploaded_file = st.file_uploader("Upload audio file", type=["wav", "mp3", "mp4"])
+uploaded_file = st.file_uploader("Upload Audio", type=["wav", "mp3", "mp4"])
 
 if uploaded_file is not None:
 
     input_path = "input_audio"
-    
     file_ext = uploaded_file.name.split(".")[-1].lower()
     input_file = f"{input_path}.{file_ext}"
 
@@ -29,24 +33,61 @@ if uploaded_file is not None:
     else:
         final_path = input_file
 
+    st.markdown("---")
+
+    st.subheader("Uploaded Audio")
     st.audio(final_path)
 
     try:
+
         features = extract_features(final_path)
 
         if features is None:
             st.error("Could not process audio")
         else:
-            features = features[np.newaxis, ..., np.newaxis]
+            features = features[np.newaxis, ...]
 
             prediction = model.predict(features)[0][0]
 
-            st.write(f"Confidence Score: {prediction:.4f}")
+            st.markdown("---")
 
-            if prediction > 0.55:
-                st.error("Fake Voice Detected")
+            st.subheader("Prediction Confidence")
+
+            col1, col2 = st.columns(2)
+
+            with col1:
+                st.metric("Fake Probability", f"{prediction:.2f}")
+
+            with col2:
+                st.metric("Real Probability", f"{1 - prediction:.2f}")
+
+            st.progress(float(prediction))
+
+            st.markdown("---")
+            st.subheader("Result")
+
+            if prediction > 0.6:
+                st.error("FAKE VOICE DETECTED")
             else:
-                st.success("Real Voice Detected")
+                st.success("REAL VOICE DETECTED")
+
+            y, sr = librosa.load(final_path)
+            duration = len(y) / sr
+
+            st.markdown("---")
+            st.subheader("Audio Information")
+
+            st.write(f"**Duration:** {duration:.2f} sec")
+            st.write(f"**Sample Rate:** {sr}")
+
+            st.markdown("---")
+            st.subheader("Spectrogram Visualization")
+
+            plt.figure(figsize=(6,4))
+            plt.imshow(features[0], aspect='auto', origin='lower')
+            plt.title("Mel Spectrogram")
+            plt.colorbar()
+            st.pyplot(plt)
 
     except Exception as e:
         st.error(f"Error: {e}")
